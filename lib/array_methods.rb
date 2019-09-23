c158827cd53c7b3c6ef48f37bf1ae92d4d7a12ce
Rommel Haynes <rhaynes3k@gmail.com>
2019-09-23 05:10:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,24 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  new_array = array.sort(array)
+  puts new_array
 end
 
 def using_reverse(array)
-
+  array.reverse(array)
 end
 
 def using_first(array)
-
+  array.first(array)
 end
 
 def using_last(array)
-
+  array.last(array)
 end
-
+  
 def using_size(array)
-
+  array.size(array)
 end
